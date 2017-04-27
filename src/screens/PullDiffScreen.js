6b2e7f66a39cd14241ccf0d50be87a783f385263
housseindjirdeh <h.djirdeh@mail.utoronto.ca>
2017-04-27 18:27:09 -0400

    update pull diff title for files changed

--- src/screens/PullDiffScreen.js
+++ src/screens/PullDiffScreen.js
@@ -1,25 +1,26 @@
 import React, {Component, PropTypes} from 'react';
-import {View, FlatList} from 'react-native';
-import {Card, ListItem} from 'react-native-elements';
+import {View, ScrollView, Text, FlatList, StyleSheet} from 'react-native';
+import {Card} from 'react-native-elements';
 
 import Parse from 'parse-diff';
 
 import ViewContainer from '../components/ViewContainer';
+import DiffBlocks from '../components/DiffBlocks';
 import CodeLine from '../components/CodeLine';
+
+import colors from '../config/colors';
 
 class PullDiff extends Component {
   componentDidMount() {
     const files = Parse(this.props.navigation.state.params.diff);
 
     console.log(files);
+  }
 
-    files.forEach(function(file) {
-      console.log(file.chunks.length); // number of hunks
-      console.log(file.chunks[0].changes.length); // hunk added/deleted/context lines
-      // each item in changes is a string
-      console.log(file.deletions); // number of deletions in the patch
-      console.log(file.additions); // number of additions in the patch
-    });
+  renderFileTitle = (title) => {
+    <View>
+     <Text>{title}</Text>
+    </View>
   }
 
   renderItem = ({item}) => {
@@ -36,8 +37,21 @@ class PullDiff extends Component {
     });
 
     return (
-      <Card containerStyle={{padding: 0}} titleStyle={{margin: 0}} title={item.from}>
-        {chunks}
+      <Card
+        containerStyle={styles.fileChangeContainer}
+        dividerStyle={styles.dividerStyle}>
+          <ScrollView
+            style={styles.fileTitleContainer}
+            automaticallyAdjustContentInsets={false}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}>
+              <View style={styles.linesChanged}>
+                <Text style={styles.codeStyle}>{item.additions + item.deletions}</Text>
+              </View>
+
+              <Text style={[styles.fileTitle, styles.codeStyle]}>{item.from}</Text>
+          </ScrollView>
+          {chunks}
       </Card>
     );
   };
@@ -66,5 +80,33 @@ class PullDiff extends Component {
 PullDiff.propTypes = {
   diff: PropTypes.string,
 };
+
+const styles = StyleSheet.create({
+  fileChangeContainer: {
+    padding: 0,
+  },
+  fileTitleContainer: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    backgroundColor: colors.greyVeryLight,
+  },
+  linesChanged: {
+    flex: 0.15,
+    width: 50,
+    paddingLeft: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  fileTitle: {
+    flex: 1,
+  },
+  codeStyle: {
+    fontFamily: 'Menlo',
+    fontSize: 12,
+  },
+  dividerStyle: {
+    marginBottom: 0,
+  },
+});
 
 export default PullDiff;
