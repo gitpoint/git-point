36d78a17281d939c702185bede5d17b9f4037cbf
June Domingo <junedomingo@gmail.com>
2017-07-16 15:51:41 +0800

    Add react-native-config

--- android/app/src/main/java/com/gitpoint/MainApplication.java
+++ android/app/src/main/java/com/gitpoint/MainApplication.java
@@ -3,6 +3,7 @@ package com.gitpoint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new RNDeviceInfo(),
             new VectorIconsPackage()
