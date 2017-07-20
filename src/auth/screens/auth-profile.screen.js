f52e2f674ed5db8f5df9046b3414970f2b489250
Sammy Israwi <sammy.israwi@gmail.com>
2017-07-20 01:14:03 -0400

    Pull down to refresh implemented in profile page

--- src/auth/screens/auth-profile.screen.js
+++ src/auth/screens/auth-profile.screen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView, RefreshControl } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import Communications from 'react-native-communications';
 
@@ -60,90 +60,105 @@ class AuthProfile extends Component {
     return url.substr(0, prefix.length) === prefix ? url : `http://${url}`;
   };
 
+  refreshProfile = () => {
+    this.props.getUser();
+    this.props.getOrgs();
+  };
+
   render() {
     const { user, orgs, isPendingUser, isPendingOrgs, navigation } = this.props;
     const loading = isPendingUser || isPendingOrgs;
 
     return (
       <ViewContainer>
-        {loading && <LoadingContainer animating={loading} center />}
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={loading}
+              onRefresh={this.refreshProfile}
+            />
+          }
+        >
+          {loading && <LoadingContainer animating={loading} center />}
 
-        {!loading &&
-          <ParallaxScroll
-            renderContent={() =>
-              <UserProfile
-                type="user"
-                initialUser={user}
-                user={user}
-                navigation={navigation}
-              />}
-            stickyTitle={user.login}
-          >
-            {user.bio &&
-              user.bio !== '' &&
-              <SectionList title="BIO">
-                <ListItem
-                  subtitle={user.bio}
-                  subtitleStyle={styles.listSubTitle}
-                  hideChevron
-                />
-              </SectionList>}
-
-            <SectionList
-              title="EMAIL"
-              noItems={!user.email || user.email === ''}
-              noItemsMessage={'No email associated with account'}
-            >
-              <ListItem
-                title="Email"
-                titleStyle={styles.listTitle}
-                leftIcon={{
-                  name: 'mail',
-                  color: colors.grey,
-                  type: 'octicon',
-                }}
-                subtitle={user.email}
-                subtitleStyle={styles.listSubTitle}
-                onPress={() =>
-                  Communications.email([user.email], null, null, 'Hi!', '')}
-                underlayColor={colors.greyLight}
-              />
-            </SectionList>
-
-            <SectionList
-              title="WEBSITE"
-              noItems={!user.blog || user.blog === ''}
-              noItemsMessage={'No website associated with account'}
-            >
-              <ListItem
-                title="Website"
-                titleStyle={styles.listTitle}
-                leftIcon={{
-                  name: 'link',
-                  color: colors.grey,
-                  type: 'octicon',
-                }}
-                subtitle={user.blog}
-                subtitleStyle={styles.listSubTitle}
-                onPress={() => Communications.web(this.getUserBlog(user.blog))}
-                underlayColor={colors.greyLight}
-              />
-            </SectionList>
-
-            <SectionList
-              title="ORGANIZATIONS"
-              noItems={orgs.length === 0}
-              noItemsMessage={'No organizations'}
-            >
-              {orgs.map(item =>
-                <UserListItem
-                  key={item.id}
-                  user={item}
+          {!loading &&
+            <ParallaxScroll
+              renderContent={() =>
+                <UserProfile
+                  type="user"
+                  initialUser={user}
+                  user={user}
                   navigation={navigation}
+                />}
+              stickyTitle={user.login}
+            >
+              {user.bio &&
+                user.bio !== '' &&
+                <SectionList title="BIO">
+                  <ListItem
+                    subtitle={user.bio}
+                    subtitleStyle={styles.listSubTitle}
+                    hideChevron
+                  />
+                </SectionList>}
+
+              <SectionList
+                title="EMAIL"
+                noItems={!user.email || user.email === ''}
+                noItemsMessage={'No email associated with account'}
+              >
+                <ListItem
+                  title="Email"
+                  titleStyle={styles.listTitle}
+                  leftIcon={{
+                    name: 'mail',
+                    color: colors.grey,
+                    type: 'octicon',
+                  }}
+                  subtitle={user.email}
+                  subtitleStyle={styles.listSubTitle}
+                  onPress={() =>
+                    Communications.email([user.email], null, null, 'Hi!', '')}
+                  underlayColor={colors.greyLight}
                 />
-              )}
-            </SectionList>
-          </ParallaxScroll>}
+              </SectionList>
+
+              <SectionList
+                title="WEBSITE"
+                noItems={!user.blog || user.blog === ''}
+                noItemsMessage={'No website associated with account'}
+              >
+                <ListItem
+                  title="Website"
+                  titleStyle={styles.listTitle}
+                  leftIcon={{
+                    name: 'link',
+                    color: colors.grey,
+                    type: 'octicon',
+                  }}
+                  subtitle={user.blog}
+                  subtitleStyle={styles.listSubTitle}
+                  onPress={() =>
+                    Communications.web(this.getUserBlog(user.blog))}
+                  underlayColor={colors.greyLight}
+                />
+              </SectionList>
+
+              <SectionList
+                title="ORGANIZATIONS"
+                noItems={orgs.length === 0}
+                noItemsMessage={'No organizations'}
+              >
+                {orgs.map(item =>
+                  <UserListItem
+                    key={item.id}
+                    user={item}
+                    navigation={navigation}
+                  />
+                )}
+              </SectionList>
+            </ParallaxScroll>}
+        </ScrollView>
       </ViewContainer>
     );
   }
