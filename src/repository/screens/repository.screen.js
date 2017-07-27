288a4f524ae10f74ed2abd3ec1fa470c774d0478
Rolf Koenders <post@madebyrolf.nl>
2017-07-28 01:32:28 +0200

    Feature: Share repository (#173)
    * Add share action to repository screen
    
    * PR feedback changes
    
    * Optimize actions array for actionsheet

--- src/repository/screens/repository.screen.js
+++ src/repository/screens/repository.screen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Share } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import ActionSheet from 'react-native-actionsheet';
 
@@ -109,13 +109,29 @@ class Repository extends Component {
         repository.name,
         starred
       );
-    }
-
-    if (index === 1 && showFork) {
+    } else if (index === 1 && showFork) {
       forkRepoByDispatch(repository.owner.login, repository.name).then(json => {
         navigation.navigate('Repository', { repository: json });
       });
+    } else if (index === 2 || (index === 1 && !showFork)) {
+      this.shareRepository(repository);
     }
+  };
+
+  shareRepository = repository => {
+    const title = `Share ${repository.name}`;
+
+    Share.share(
+      {
+        title,
+        message: `Check out ${repository.name} on Github. ${repository.html_url}`,
+        url: undefined,
+      },
+      {
+        dialogTitle: title,
+        excludedActivityTypes: [],
+      }
+    );
   };
 
   render() {
@@ -145,13 +161,13 @@ class Repository extends Component {
     const openIssues = pureIssues.filter(issue => issue.state === 'open');
     const closedIssues = pureIssues.filter(issue => issue.state === 'closed');
 
-    const repositoryActions = [starred ? '★ Unstar' : '★ Star'];
     const showFork =
       repository && repository.owner && repository.owner.login !== username;
-
-    if (showFork) {
-      repositoryActions.push('Fork');
-    }
+    const repositoryActions = [
+      starred ? 'Unstar' : 'Star',
+      showFork && 'Fork',
+      'Share',
+    ];
 
     const loader = isPendingFork ? <LoadingModal /> : null;
 
@@ -205,7 +221,8 @@ class Repository extends Component {
               />
             </SectionList>}
 
-          {(isPendingRepository || isPendingContributors) && <LoadingMembersList title="CONTRIBUTORS" />}
+          {(isPendingRepository || isPendingContributors) &&
+            <LoadingMembersList title="CONTRIBUTORS" />}
 
           {!isPendingContributors &&
             <MembersList
