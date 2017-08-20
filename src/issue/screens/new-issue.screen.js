6863271053340620a9677a92dd81cc516aeca776
Antoine Boisadam <Antoine38660@users.noreply.github.com>
2017-08-20 02:05:10 +0200

    fix(new-issue): various submit new issue improvement (#243)
    * fix(issue): hide back btn after submitting a new issue - fix #236
    
    * fix(issue): ability to add newline in issue body
    
    * fix(submit newissue): show a loading modal after submiting a new issue
    
    * fix: disable swipe back AND header leftbtn after when submit an issue

--- src/issue/screens/new-issue.screen.js
+++ src/issue/screens/new-issue.screen.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { ScrollView, StyleSheet, TextInput, View, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
-import { ViewContainer, SectionList } from 'components';
+import { ViewContainer, SectionList, LoadingModal } from 'components';
 import { translate } from 'utils';
 import { colors, fonts, normalize } from 'config';
 import { submitNewIssue } from '../issue.action';
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
 const mapStateToProps = state => ({
   language: state.auth.language,
   repository: state.repository.repository,
+  isPendingSubmitting: state.issue.isPendingSubmitting,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -46,6 +47,7 @@ class NewIssue extends Component {
     language: string,
     repository: Object,
     navigation: Object,
+    isPendingSubmitting: boolean,
   };
 
   state: {
@@ -90,17 +92,20 @@ class NewIssue extends Component {
       ).then(issue => {
         navigation.navigate('Issue', {
           issue,
+          headerLeft: null,
+          gesturesEnabled: false,
         });
       });
     }
   };
 
   render() {
-    const { language, repository } = this.props;
+    const { language, repository, isPendingSubmitting } = this.props;
     const { issueTitle, issueComment } = this.state;
 
     return (
       <ViewContainer>
+        {isPendingSubmitting && <LoadingModal />}
         <ScrollView>
           {repository.full_name &&
             <ListItem
@@ -140,7 +145,6 @@ class NewIssue extends Component {
             <TextInput
               underlineColorAndroid={'transparent'}
               placeholder={translate('issue.newIssue.writeAComment', language)}
-              blurOnSubmit
               multiline
               onChangeText={text => this.setState({ issueComment: text })}
               onContentSizeChange={event =>
