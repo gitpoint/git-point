d28019ed41c7dcfa3af0ce7d4c41a45623145c19
Adrian <adrianasdfs@gmail.com>
2017-08-22 13:45:36 +0800

    feat(star-count) add requestStarCount to get and parse star count

--- src/api/index.js
+++ src/api/index.js
@@ -310,6 +310,48 @@ export async function fetchForkRepo(owner, repo, accessToken) {
   return response;
 }
 
+export const requestStarCount = owner => {
+  const ENDPOINT = `https://api.github.com/users/${owner}/starred?per_page=1`;
+
+  const requestPromise = new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+
+    req.open('GET', ENDPOINT);
+    req.onerror = () => reject(req.statusText);
+
+    req.onreadystatechange = () => {
+      if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+        let linkHeader = req.getResponseHeader('Link');
+        let output = '';
+
+        if (linkHeader == null) {
+          const responseBody = JSON.parse(req.response);
+
+          output = responseBody.length;
+        } else {
+          linkHeader = linkHeader.match(/page=(\d)+/g).pop();
+          output = linkHeader.split('=').pop();
+        }
+
+        // Add 'k' if star more than 1000
+
+        if (output > 1000) {
+          const outDecimal = (output / 1000).toString();
+          const dotIndex = outDecimal.indexOf('.');
+
+          output = `${outDecimal.substring(0, dotIndex + 3)}k`;
+        }
+
+        resolve(output);
+      }
+    };
+
+    req.send();
+  });
+
+  return requestPromise;
+};
+
 export async function watchRepo(isSubscribed, owner, repo, accessToken) {
   const ENDPOINT = `https://api.github.com/repos/${owner}/${repo}/subscription`;
   const response = await fetch(
