68ebd4fa450b9295fc16a17ba89aeaf4422dad16
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-10-01 00:10:57 -0700

    test: add more tests

--- testenv.js
+++ testenv.js
@@ -19,10 +19,10 @@ jest.mock('react-native-i18n', () => {
   const i18njs = require('i18n-js');
   const en = require('./src/locale/languages/en');
 
-  i18njs.translations = { en };
+  i18njs.translations = en;
 
   return {
-    t: jest.fn((k, o) => i18njs.t(k, o)),
+    t: jest.fn((k, o) => i18njs.t(k, { locale: 'en' })),
   };
 });
 
