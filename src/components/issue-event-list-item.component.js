332ea33a7249dd19fb0c90224f8f4a12fe555275
brandly <brandly13@gmail.com>
2017-10-05 02:33:37 -0400

    style(issue_events): Added styles to "added label" event

--- src/components/issue-event-list-item.component.js
+++ src/components/issue-event-list-item.component.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 import moment from 'moment/min/moment-with-locales.min';
+import { colors, fonts, normalize } from 'config';
+import { LabelButton } from 'components';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: 10,
+    paddingTop: 10,
+    backgroundColor: 'transparent',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  actorLink: {
+    padding: 1,
+    paddingRight: 4,
+  },
+  dateContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    alignSelf: 'flex-start',
+    marginTop: 2,
+  },
+  linkDescription: {
+    ...fonts.fontPrimaryBold,
+    fontSize: normalize(13),
+    color: colors.primaryDark,
+  },
+  date: {
+    color: colors.greyDark,
+  },
+  textContainer: {
+    marginLeft: 54,
+    borderBottomColor: colors.greyLight,
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    flexDirection: 'row',
+  },
+});
 
 export class IssueEventListItem extends Component {
   props: {
@@ -30,7 +71,7 @@ class ReviewRequested extends Component {
     const { review_requester, requested_reviewer } = this.props.event;
 
     return (
-      <View>
+      <View style={styles.container}>
         <Text>
           {review_requester.login} requested review from{' '}
           {requested_reviewer.login}
@@ -40,21 +81,74 @@ class ReviewRequested extends Component {
   }
 }
 
-class Labeled extends Component {
+class LabeledComponent extends Component {
   props: {
     event: Object,
+    // authUser: Object,
   };
 
   render() {
-    const { actor, label, created_at } = this.props.event;
+    const {
+      actor,
+      label,
+      created_at,
+      // navigation,
+      // authUser,
+    } = this.props.event;
 
     return (
-      <View>
-        <Text>
-          {actor.login} added {label.name} (#{label.color}) label{' '}
-          {moment(created_at).fromNow()}
-        </Text>
+      <View style={[styles.container, styles.header]}>
+        <View style={styles.textContainer}>
+          <TouchableOpacity
+            style={styles.actorLink}
+            onPress={() => {
+              // TODO: figure out navigation
+              // navigation.navigate(
+              //   authUser.login === actor.login
+              //     ? 'AuthProfile'
+              //     : 'Profile',
+              //   {
+              //     user: actor,
+              //   },
+              // )
+            }}
+          >
+            <Text style={styles.linkDescription}>
+              {actor.login}
+            </Text>
+          </TouchableOpacity>
+          <Text
+            style={{
+              padding: 3,
+              paddingRight: 6,
+              paddingLeft: 0,
+            }}
+          >
+            added the
+          </Text>
+          <LabelButton label={label} />
+          <Text
+            style={{
+              padding: 3,
+              paddingLeft: 6,
+              paddingRight: 0,
+            }}
+          >
+            label
+          </Text>
+        </View>
+        <View style={styles.dateContainer}>
+          <Text style={styles.date}>
+            {moment(created_at).fromNow()}
+          </Text>
+        </View>
       </View>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  authUser: state.auth.user,
+});
+
+const Labeled = connect(mapStateToProps)(LabeledComponent);
