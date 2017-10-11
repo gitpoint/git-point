0f06192f06135d289e6aadd4386b17b6959405dc
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2017-10-11 16:03:05 +0200

    feat(translation): Add Polish translation (#466)
    This commits adds files required to provide a Polish translation
    strings for the GitPoint. Tested locally using iOS simulator.
    
    Thanks!

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native';
 import I18n from 'react-native-i18n';
-import { en, fr, nl, pt, ptBr, tr, ru, gl } from './languages';
+import { en, fr, nl, pt, ptBr, tr, ru, gl, pl } from './languages';
 
 I18n.fallbacks = true;
 
@@ -13,6 +13,7 @@ I18n.translations = {
   'pt-br': ptBr,
   ru,
   gl,
+  pl,
 };
 
 export default I18n;
