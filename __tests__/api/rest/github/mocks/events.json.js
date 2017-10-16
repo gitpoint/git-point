40429ca69ce9d009f2d6fb9b8548d3fe085a9368
Mehdi Achour <machour@gmail.com>
2017-10-16 23:19:34 +0100

    test: Implement all schemas tests

---
+++ __tests__/api/rest/github/mocks/events.json.js
@@ -0,0 +1,220 @@
+export default [
+  {
+    id: '6723243870',
+    type: 'IssueCommentEvent',
+    actor: {
+      id: 32770098,
+      login: 'machour2',
+      display_login: 'machour2',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/machour2',
+      avatar_url: 'https://avatars.githubusercontent.com/u/32770098?',
+    },
+    repo: {
+      id: 103052187,
+      name: 'machour/git-point-playground',
+      url: 'https://api.github.com/repos/machour/git-point-playground',
+    },
+    payload: {
+      action: 'created',
+      issue: {
+        url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/14',
+        repository_url:
+          'https://api.github.com/repos/machour/git-point-playground',
+        labels_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/14/labels{/name}',
+        comments_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/14/comments',
+        events_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/14/events',
+        html_url: 'https://github.com/machour/git-point-playground/issues/14',
+        id: 265577462,
+        number: 14,
+        title: 'more test',
+        user: {
+          login: 'machour2',
+          id: 32770098,
+          avatar_url: 'https://avatars0.githubusercontent.com/u/32770098?v=4',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/machour2',
+          html_url: 'https://github.com/machour2',
+          followers_url: 'https://api.github.com/users/machour2/followers',
+          following_url:
+            'https://api.github.com/users/machour2/following{/other_user}',
+          gists_url: 'https://api.github.com/users/machour2/gists{/gist_id}',
+          starred_url:
+            'https://api.github.com/users/machour2/starred{/owner}{/repo}',
+          subscriptions_url:
+            'https://api.github.com/users/machour2/subscriptions',
+          organizations_url: 'https://api.github.com/users/machour2/orgs',
+          repos_url: 'https://api.github.com/users/machour2/repos',
+          events_url: 'https://api.github.com/users/machour2/events{/privacy}',
+          received_events_url:
+            'https://api.github.com/users/machour2/received_events',
+          type: 'User',
+          site_admin: false,
+        },
+        labels: [],
+        state: 'open',
+        locked: false,
+        assignee: null,
+        assignees: [],
+        milestone: null,
+        comments: 1,
+        created_at: '2017-10-15T14:48:14Z',
+        updated_at: '2017-10-16T17:53:19Z',
+        closed_at: null,
+        author_association: 'NONE',
+        body: '',
+      },
+      comment: {
+        url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/comments/336970278',
+        html_url:
+          'https://github.com/machour/git-point-playground/issues/14#issuecomment-336970278',
+        issue_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/14',
+        id: 336970278,
+        user: {
+          login: 'machour2',
+          id: 32770098,
+          avatar_url: 'https://avatars0.githubusercontent.com/u/32770098?v=4',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/machour2',
+          html_url: 'https://github.com/machour2',
+          followers_url: 'https://api.github.com/users/machour2/followers',
+          following_url:
+            'https://api.github.com/users/machour2/following{/other_user}',
+          gists_url: 'https://api.github.com/users/machour2/gists{/gist_id}',
+          starred_url:
+            'https://api.github.com/users/machour2/starred{/owner}{/repo}',
+          subscriptions_url:
+            'https://api.github.com/users/machour2/subscriptions',
+          organizations_url: 'https://api.github.com/users/machour2/orgs',
+          repos_url: 'https://api.github.com/users/machour2/repos',
+          events_url: 'https://api.github.com/users/machour2/events{/privacy}',
+          received_events_url:
+            'https://api.github.com/users/machour2/received_events',
+          type: 'User',
+          site_admin: false,
+        },
+        created_at: '2017-10-16T17:53:19Z',
+        updated_at: '2017-10-16T17:53:19Z',
+        author_association: 'NONE',
+        body: 'plop',
+      },
+    },
+    public: true,
+    created_at: '2017-10-16T17:53:19Z',
+  },
+  {
+    id: '6723241817',
+    type: 'IssueCommentEvent',
+    actor: {
+      id: 32770098,
+      login: 'machour2',
+      display_login: 'machour2',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/machour2',
+      avatar_url: 'https://avatars.githubusercontent.com/u/32770098?',
+    },
+    repo: {
+      id: 103052187,
+      name: 'machour/git-point-playground',
+      url: 'https://api.github.com/repos/machour/git-point-playground',
+    },
+    payload: {
+      action: 'created',
+      issue: {
+        url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/15',
+        repository_url:
+          'https://api.github.com/repos/machour/git-point-playground',
+        labels_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/15/labels{/name}',
+        comments_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/15/comments',
+        events_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/15/events',
+        html_url: 'https://github.com/machour/git-point-playground/issues/15',
+        id: 265851173,
+        number: 15,
+        title: 'notif',
+        user: {
+          login: 'machour2',
+          id: 32770098,
+          avatar_url: 'https://avatars0.githubusercontent.com/u/32770098?v=4',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/machour2',
+          html_url: 'https://github.com/machour2',
+          followers_url: 'https://api.github.com/users/machour2/followers',
+          following_url:
+            'https://api.github.com/users/machour2/following{/other_user}',
+          gists_url: 'https://api.github.com/users/machour2/gists{/gist_id}',
+          starred_url:
+            'https://api.github.com/users/machour2/starred{/owner}{/repo}',
+          subscriptions_url:
+            'https://api.github.com/users/machour2/subscriptions',
+          organizations_url: 'https://api.github.com/users/machour2/orgs',
+          repos_url: 'https://api.github.com/users/machour2/repos',
+          events_url: 'https://api.github.com/users/machour2/events{/privacy}',
+          received_events_url:
+            'https://api.github.com/users/machour2/received_events',
+          type: 'User',
+          site_admin: false,
+        },
+        labels: [],
+        state: 'open',
+        locked: false,
+        assignee: null,
+        assignees: [],
+        milestone: null,
+        comments: 0,
+        created_at: '2017-10-16T17:31:03Z',
+        updated_at: '2017-10-16T17:52:54Z',
+        closed_at: null,
+        author_association: 'NONE',
+        body: '',
+      },
+      comment: {
+        url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/comments/336970090',
+        html_url:
+          'https://github.com/machour/git-point-playground/issues/15#issuecomment-336970090',
+        issue_url:
+          'https://api.github.com/repos/machour/git-point-playground/issues/15',
+        id: 336970090,
+        user: {
+          login: 'machour2',
+          id: 32770098,
+          avatar_url: 'https://avatars0.githubusercontent.com/u/32770098?v=4',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/machour2',
+          html_url: 'https://github.com/machour2',
+          followers_url: 'https://api.github.com/users/machour2/followers',
+          following_url:
+            'https://api.github.com/users/machour2/following{/other_user}',
+          gists_url: 'https://api.github.com/users/machour2/gists{/gist_id}',
+          starred_url:
+            'https://api.github.com/users/machour2/starred{/owner}{/repo}',
+          subscriptions_url:
+            'https://api.github.com/users/machour2/subscriptions',
+          organizations_url: 'https://api.github.com/users/machour2/orgs',
+          repos_url: 'https://api.github.com/users/machour2/repos',
+          events_url: 'https://api.github.com/users/machour2/events{/privacy}',
+          received_events_url:
+            'https://api.github.com/users/machour2/received_events',
+          type: 'User',
+          site_admin: false,
+        },
+        created_at: '2017-10-16T17:52:54Z',
+        updated_at: '2017-10-16T17:52:54Z',
+        author_association: 'NONE',
+        body: 'bla',
+      },
+    },
+    public: true,
+    created_at: '2017-10-16T17:52:54Z',
+  },
+];
---
+++ __tests__/api/rest/github/mocks/repo.json.js
@@ -0,0 +1,133 @@
+export default {
+  id: 86202845,
+  name: 'git-point',
+  full_name: 'gitpoint/git-point',
+  owner: {
+    login: 'gitpoint',
+    id: 30082377,
+    avatar_url: 'https://avatars0.githubusercontent.com/u/30082377?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/gitpoint',
+    html_url: 'https://github.com/gitpoint',
+    followers_url: 'https://api.github.com/users/gitpoint/followers',
+    following_url:
+      'https://api.github.com/users/gitpoint/following{/other_user}',
+    gists_url: 'https://api.github.com/users/gitpoint/gists{/gist_id}',
+    starred_url: 'https://api.github.com/users/gitpoint/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/gitpoint/subscriptions',
+    organizations_url: 'https://api.github.com/users/gitpoint/orgs',
+    repos_url: 'https://api.github.com/users/gitpoint/repos',
+    events_url: 'https://api.github.com/users/gitpoint/events{/privacy}',
+    received_events_url:
+      'https://api.github.com/users/gitpoint/received_events',
+    type: 'Organization',
+    site_admin: false,
+  },
+  private: false,
+  html_url: 'https://github.com/gitpoint/git-point',
+  description: 'GitHub in your pocket :iphone:',
+  fork: false,
+  url: 'https://api.github.com/repos/gitpoint/git-point',
+  forks_url: 'https://api.github.com/repos/gitpoint/git-point/forks',
+  keys_url: 'https://api.github.com/repos/gitpoint/git-point/keys{/key_id}',
+  collaborators_url:
+    'https://api.github.com/repos/gitpoint/git-point/collaborators{/collaborator}',
+  teams_url: 'https://api.github.com/repos/gitpoint/git-point/teams',
+  hooks_url: 'https://api.github.com/repos/gitpoint/git-point/hooks',
+  issue_events_url:
+    'https://api.github.com/repos/gitpoint/git-point/issues/events{/number}',
+  events_url: 'https://api.github.com/repos/gitpoint/git-point/events',
+  assignees_url:
+    'https://api.github.com/repos/gitpoint/git-point/assignees{/user}',
+  branches_url:
+    'https://api.github.com/repos/gitpoint/git-point/branches{/branch}',
+  tags_url: 'https://api.github.com/repos/gitpoint/git-point/tags',
+  blobs_url: 'https://api.github.com/repos/gitpoint/git-point/git/blobs{/sha}',
+  git_tags_url:
+    'https://api.github.com/repos/gitpoint/git-point/git/tags{/sha}',
+  git_refs_url:
+    'https://api.github.com/repos/gitpoint/git-point/git/refs{/sha}',
+  trees_url: 'https://api.github.com/repos/gitpoint/git-point/git/trees{/sha}',
+  statuses_url:
+    'https://api.github.com/repos/gitpoint/git-point/statuses/{sha}',
+  languages_url: 'https://api.github.com/repos/gitpoint/git-point/languages',
+  stargazers_url: 'https://api.github.com/repos/gitpoint/git-point/stargazers',
+  contributors_url:
+    'https://api.github.com/repos/gitpoint/git-point/contributors',
+  subscribers_url:
+    'https://api.github.com/repos/gitpoint/git-point/subscribers',
+  subscription_url:
+    'https://api.github.com/repos/gitpoint/git-point/subscription',
+  commits_url: 'https://api.github.com/repos/gitpoint/git-point/commits{/sha}',
+  git_commits_url:
+    'https://api.github.com/repos/gitpoint/git-point/git/commits{/sha}',
+  comments_url:
+    'https://api.github.com/repos/gitpoint/git-point/comments{/number}',
+  issue_comment_url:
+    'https://api.github.com/repos/gitpoint/git-point/issues/comments{/number}',
+  contents_url:
+    'https://api.github.com/repos/gitpoint/git-point/contents/{+path}',
+  compare_url:
+    'https://api.github.com/repos/gitpoint/git-point/compare/{base}...{head}',
+  merges_url: 'https://api.github.com/repos/gitpoint/git-point/merges',
+  archive_url:
+    'https://api.github.com/repos/gitpoint/git-point/{archive_format}{/ref}',
+  downloads_url: 'https://api.github.com/repos/gitpoint/git-point/downloads',
+  issues_url: 'https://api.github.com/repos/gitpoint/git-point/issues{/number}',
+  pulls_url: 'https://api.github.com/repos/gitpoint/git-point/pulls{/number}',
+  milestones_url:
+    'https://api.github.com/repos/gitpoint/git-point/milestones{/number}',
+  notifications_url:
+    'https://api.github.com/repos/gitpoint/git-point/notifications{?since,all,participating}',
+  labels_url: 'https://api.github.com/repos/gitpoint/git-point/labels{/name}',
+  releases_url: 'https://api.github.com/repos/gitpoint/git-point/releases{/id}',
+  deployments_url:
+    'https://api.github.com/repos/gitpoint/git-point/deployments',
+  created_at: '2017-03-26T02:45:46Z',
+  updated_at: '2017-10-16T17:15:40Z',
+  pushed_at: '2017-10-16T20:44:40Z',
+  git_url: 'git://github.com/gitpoint/git-point.git',
+  ssh_url: 'git@github.com:gitpoint/git-point.git',
+  clone_url: 'https://github.com/gitpoint/git-point.git',
+  svn_url: 'https://github.com/gitpoint/git-point',
+  homepage: 'https://gitpoint.co/',
+  size: 3798,
+  stargazers_count: 2501,
+  watchers_count: 2501,
+  language: 'JavaScript',
+  has_issues: true,
+  has_projects: true,
+  has_downloads: true,
+  has_wiki: true,
+  has_pages: false,
+  forks_count: 224,
+  mirror_url: null,
+  open_issues_count: 93,
+  forks: 224,
+  open_issues: 93,
+  watchers: 2501,
+  default_branch: 'master',
+  organization: {
+    login: 'gitpoint',
+    id: 30082377,
+    avatar_url: 'https://avatars0.githubusercontent.com/u/30082377?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/gitpoint',
+    html_url: 'https://github.com/gitpoint',
+    followers_url: 'https://api.github.com/users/gitpoint/followers',
+    following_url:
+      'https://api.github.com/users/gitpoint/following{/other_user}',
+    gists_url: 'https://api.github.com/users/gitpoint/gists{/gist_id}',
+    starred_url: 'https://api.github.com/users/gitpoint/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/gitpoint/subscriptions',
+    organizations_url: 'https://api.github.com/users/gitpoint/orgs',
+    repos_url: 'https://api.github.com/users/gitpoint/repos',
+    events_url: 'https://api.github.com/users/gitpoint/events{/privacy}',
+    received_events_url:
+      'https://api.github.com/users/gitpoint/received_events',
+    type: 'Organization',
+    site_admin: false,
+  },
+  network_count: 224,
+  subscribers_count: 50,
+};
