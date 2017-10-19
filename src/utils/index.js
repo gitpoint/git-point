3ec19348ab060800b5b2a1f97fbbca2b6bcc3a69
Matthew Brandly <brandly13@gmail.com>
2017-10-18 20:47:57 -0400

    feat: Issue Events (#438)
    * feat(issue_events): Show events on issues
    
    * style(issue_events): Added styles to "added label" event
    
    * style(issue_events): Add icon & improve styling of added labels
    
    * style(issue_events): Improve <ReviewRequested /> styles
    
    * feat(issue_events): Remove mentioned/subscribed events from UI
    
    * feat(issue_events): Define <Closed /> events
    
    * refactor(issue_events): Extract <EventIcon /> and <Date />
    
    * feat(issue_events): Add `unlabled` prop to <Labeled />
    
    * feat(issue_events): Define <Merged /> event
    
    * feat(issue_events): Filter out `closed` events preceded by `merged`
    
    * feat(issue_events): Define <HeadRef /> events
    
    * feat(issue_events): Define <Assigned /> events
    
    * feat(issue_events): Define <Reopened /> & <Renamed /> events
    
    * refactor(issue_events): Render <Text /> from <ActorLink />
    
    * style(issue_events): Trim issue names to ensure spacing
    
    * feat(issue_events): Define <Locked />  event
    
    * feat(issue_events): Define <Milestoned />  event
    
    * refactor(issue_events): Clean up authUser from LabeledComponent
    
    * feat(issue_events): Define <MarkedAsDuplicate /> event
    
    * refactor(issue_events): Define generic <Event /> component
    
    * docs(readme): Add @brandly as a contributor
    
    * feat(issue_events): Define <LabelGroup /> for list of label changes
    
    * refactor(issue_events): Use spread operator for textChildren
    
    * style(issue_events): Add blank line after external imports
    
    * feat(issue_events): <InlineLabel /> has rounded corners
    
    * refactor(issue_events): Move <InlineLabel /> into own file
    
    * feat(issue_events): Press username in events to view profile
    
    * refactor(events): Inline most <Event />s into <IssueEventListItem />
    
    * refactor(events): Eliminate <Date /> since its only used once
    
    * refactor(events): Extract formatEventsToRender into event-helpers

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,6 @@ export * from './action-helper';
 export * from './loading-animation';
 export * from './text-helper';
 export * from './method-helpers';
+export * from './color-helpers';
+export * from './event-helpers';
 export * from './localization-helper';
