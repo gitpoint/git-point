a2616caae7fa9c322fc39bc467e1d98edb68f99d
Mehdi Achour <machour@gmail.com>
2017-10-21 02:36:55 +0100

    chore(*): convert diff-blocks.component to styled component

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -6,3 +6,12 @@ export const fonts = {
   fontPrimaryBold: { fontFamily: 'Nunito-Bold' },
   fontCode: { fontFamily: 'Menlo' },
 };
+
+export const fontsStyled = {
+  fontPrimaryLight: { 'font-family': 'Nunito-Light' },
+  fontPrimary: { 'font-family': 'Nunito-Regular' },
+  fontPrimaryItalic: { 'font-family': 'Nunito-Italic' },
+  fontPrimarySemiBold: { 'font-family': 'Nunito-SemiBold' },
+  fontPrimaryBold: { 'font-family': 'Nunito-Bold' },
+  fontCode: { 'font-family': 'Menlo' },
+};
