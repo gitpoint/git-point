e13354726ea3e2bbdb440aeff64021a544c71af9
JOSE NARANJO <josel@JOSEs-MacBook-Pro.local>
2017-10-22 19:07:24 -0500

    refactor(styles): Updating CodeLine component to use Styled-components
    Deleted StyledSheet styles and migrate those to Styled-Components format.
    
    515

---
+++ src/config/styled-fonts.js
@@ -0,0 +1,8 @@
+export const styledFonts = {
+  fontPrimaryLight: 'Nunito-Light',
+  fontPrimary: 'Nunito-Regular',
+  fontPrimaryItalic: 'Nunito-Italic',
+  fontPrimarySemiBold: 'Nunito-SemiBold',
+  fontPrimaryBold: 'Nunito-Bold',
+  fontCode: 'Menlo',
+};
