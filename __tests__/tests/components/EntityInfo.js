9c8ecb57e326c057d72b2d8753e1f578bb7e6047
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-10-26 12:15:31 -0700

    Add another entity info test

--- __tests__/tests/components/EntityInfo.js
+++ __tests__/tests/components/EntityInfo.js
@@ -7,7 +7,7 @@ import user from 'testData/api/user';
 import { EntityInfo } from 'components';
 
 describe('<EntityInfo />', () => {
-  it('correctly renders organization', () => {
+  it('should render organization correctly', () => {
     const wrapper = shallow(<EntityInfo entity={organization} language="en" />);
 
     expect(wrapper.find({ title: 'Location' }).prop('subtitle')).toEqual(
@@ -21,9 +21,28 @@ describe('<EntityInfo />', () => {
     );
   });
 
-  it('correctly renders users', () => {
+  it('should render users correctly', () => {
     const wrapper = shallow(
       <EntityInfo entity={user} orgs={[organization]} language="en" />
+    );
+
+    expect(wrapper.find({ title: 'Location' }).prop('subtitle')).toEqual(
+      'San Francisco'
+    );
+    expect(wrapper.find({ title: 'Email' }).prop('subtitle')).toEqual(
+      'octocat@github.com'
+    );
+    expect(wrapper.find({ title: 'Website' }).prop('subtitle')).toEqual(
+      'https://github.com/blog'
+    );
+    expect(wrapper.find({ title: 'Company' }).prop('subtitle')).toEqual(
+      '@GitHub'
+    );
+  });
+  
+  it('should render user without organizations', () => {
+    const wrapper = shallow(
+      <EntityInfo entity={user} orgs={[]} language="en" />
     );
 
     expect(wrapper.find({ title: 'Location' }).prop('subtitle')).toEqual(
