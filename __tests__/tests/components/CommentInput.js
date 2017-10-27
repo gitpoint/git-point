f478de08fdc7696404d6efa8655623fbbf6a0602
Dyesse YUMBA <yumba.nduba@gmail.com>
2017-10-27 16:11:04 +0100

    refactor: Beautify the code of CommentInput unit test.

--- __tests__/tests/components/CommentInput.js
+++ __tests__/tests/components/CommentInput.js
@@ -1,53 +1,52 @@
 import React from 'react';
-import {shallow, render} from 'enzyme';
+import {shallow} from 'enzyme';
 import sinon from 'sinon';
 import {Platform} from 'react-native';
 
 import {CommentInput} from 'components';
 
 describe('<CommentInput />', () => {
-  //If the user is connected, the component should let him push comment.
-  //If the user is not connected, the TextInput should disappear.
 
-  it("Should render 'TextInput' and TouchableOpacity if user can post", () => {
+  let defaultProps = {
+    users: [],
+    userHasPushPermission: true,
+    issueLocked: false,
+    locale: '',
+    onSubmit: () => {},
+  };
+
+  it("should render 'TextInput' and TouchableOpacity if user can post", () => {
 
     const wrapper = shallow(
-      <CommentInput
-        users={[]}
-        userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-        onSubmit={() => {
-        }}/>);
+      <CommentInput {...defaultProps}/>
+    );
 
     expect(wrapper.find('TextInput').length).toEqual(1);
     expect(wrapper.find('TouchableOpacity').length).toEqual(1);
     expect(wrapper.find('Text').length).toEqual(0);
   });
 
-  it("Should render 'TextInput' and TouchableOpacity if user can post", () => {
+  it("should not render 'TextInput' and TouchableOpacity if user can post", () => {
 
     const wrapper = shallow(
       <CommentInput
-        users={[]}
+        {...defaultProps}
         userHasPushPermission={false}
-        issueLocked={true}
-        locale=''
-        onSubmit={() => {
-        }}/>);
+        issueLocked={true}/>
+    );
 
     expect(wrapper.find('TextInput').length).toEqual(0);
     expect(wrapper.find('TouchableOpacity').length).toEqual(0);
     expect(wrapper.find('Text').length).toEqual(1);
   });
 
-  it('Should update the state text if value change', () => {
+  it('should update the state text if value change', () => {
     const wrapper = shallow(
       <CommentInput
+        {...defaultProps}
         userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-      />);
+        issueLocked={false}/>
+    );
 
     const input = wrapper.find('TextInput');
 
@@ -56,74 +55,75 @@ describe('<CommentInput />', () => {
     expect(wrapper.state('text')).toEqual('Changed text');
   });
 
-  it('Should update the state text if value change', () => {
+  it('should call handleSubmit methods when submitted', () => {
     const wrapper = shallow(
       <CommentInput
+        {...defaultProps}
         userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-      />);
-
-    wrapper.find('TextInput').simulate('changeText', 'Changed text');
-
-    expect(wrapper.state('text')).toEqual('Changed text');
-  });
-
-  it('Should call handleSubmit  methods when submitted', () => {
-    const wrapper = shallow(
-      <CommentInput
-        userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-        onSubmit={() => {
-        }}
-      />);
+        issueLocked={false}/>
+    );
 
     const handleSubmitSpy = sinon.spy(wrapper.instance(), 'handleSubmit');
 
-    wrapper.instance().forceUpdate();
+    wrapper
+      .instance()
+      .forceUpdate();
 
-    wrapper.find('TextInput').simulate('changeText', 'Changed text');
-    wrapper.find('TouchableOpacity').simulate('press');
+    wrapper
+      .find('TextInput')
+      .simulate('changeText', 'Changed text');
+
+    wrapper
+      .find('TouchableOpacity')
+      .simulate('press');
 
     expect(handleSubmitSpy.calledOnce).toBe(true);
   });
 
-  it('Should change the content size', () => {
+  it('should change the content size', () => {
     const wrapper = shallow(
       <CommentInput
+        {...defaultProps}
         userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-        onSubmit={() => {
-        }}
-      />);
+        issueLocked={false}/>
+    );
 
-    console.log(wrapper.state('height'));
-
-    //User contentSizeChange event to change the state 'height'
-    wrapper.find('TextInput').simulate('contentSizeChange', {nativeEvent: {contentSize: {height: 10}}});
+    wrapper
+      .find('TextInput')
+      .simulate('contentSizeChange', {
+        nativeEvent: {
+          contentSize: {
+            height: 10
+          }
+        }
+      });
 
     expect(wrapper.state('height')).toBe(10);
   });
 
-  it('Should call handleSubmitEditing methods when onSubmitEditing event raised', () => {
+  it('should call handleSubmitEditing methods when onSubmitEditing event raised', () => {
     const wrapper = shallow(
       <CommentInput
+        {...defaultProps}
         userHasPushPermission={true}
-        issueLocked={false}
-        locale=''
-        onSubmit={() => {
-        }}
-      />);
+        issueLocked={false}/>
+    );
 
     const handleSubmitEditingSpy = sinon.spy(wrapper.instance(), 'handleSubmitEditing');
 
-    wrapper.instance().forceUpdate();
+    wrapper
+      .instance()
+      .forceUpdate();
 
     Platform.OS = 'android';
 
-    wrapper.find('TextInput').simulate('submitEditing', {nativeEvent: {text: 'Changed by submitEditing'}});
+    wrapper
+      .find('TextInput')
+      .simulate('submitEditing', {
+        nativeEvent: {
+          text: 'Changed by submitEditing'
+        }
+      });
 
     expect(handleSubmitEditingSpy.calledOnce).toBe(true);
 
