59a39ae9516ac0f15a62ccb2e2ccb181c65d6ce4
Alejandro Nanez <alejonanez@gmail.com>
2017-10-26 20:53:26 -0500

    feat(test): add NotificationListItem component tests

---
+++ __tests__/tests/components/NotificationListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { NotificationListItem } from 'components';
+
+describe('<NotificationListItem />', () => {
+  it.skip("should render a View component if notification type is 'Commit'");
+  it.skip(
+    "should render a TouchableOpacity component if notification type is not 'Commit'"
+  );
+  it.skip('should call navigation when user press TitleComponent');
+  it.skip('should return the correct icon name');
+});
