03780c1835f442829cd942ba1d048c0ec0e92299
Mehdi Achour <machour@gmail.com>
2018-01-16 13:16:30 +0100

    chore: Upgrade react, react-dom, react-native to latests versions
    Upgrading react-native-linear-gradient is mandatory as it fixes a compatibility problem: react-native-community/react-native-linear-gradient#235

--- android/app/src/main/java/com/gitpoint/MainApplication.java
+++ android/app/src/main/java/com/gitpoint/MainApplication.java
@@ -47,6 +47,11 @@ public class MainApplication extends Application implements ReactApplication {
             new VectorIconsPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
