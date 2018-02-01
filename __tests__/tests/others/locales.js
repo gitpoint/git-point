526d31c53f98f7700037897989944548728f20e1
Mehdi Achour <machour@gmail.com>
2018-02-01 20:19:48 +0100

    test: make locale test output more readable

--- __tests__/tests/others/locales.js
+++ __tests__/tests/others/locales.js
@@ -1,28 +1,15 @@
 import { common } from 'config';
+import keysDiff from 'keys-diff';
 import * as languages from 'locale/languages';
 
-function createExpectedObject(object) {
-  const ret = {};
-
-  Object.keys(object).forEach(key => {
-    ret[key] =
-      typeof object[key] === 'object'
-        ? createExpectedObject(object[key])
-        : expect.stringMatching(/./);
-  });
-
-  return ret;
-}
-
 const baseLanguage = common.defaultLocale;
-const expectedObject = createExpectedObject(languages[baseLanguage]);
+const baseTranslation = languages[baseLanguage];
 
 describe('Locales', () => {
   Object.keys(languages).forEach(key => {
     if (key === baseLanguage) return;
-
     it(`${key} should contain same keys as ${baseLanguage}`, () => {
-      expect(languages[key]).toMatchObject(expectedObject);
+      expect(keysDiff(baseTranslation, languages[key])).toEqual([[], []]);
     });
   });
 });
