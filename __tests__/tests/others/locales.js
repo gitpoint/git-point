0a317ae81870ae605e88f31ef6720e4059b8fa0d
Mehdi Achour <machour@gmail.com>
2018-02-02 16:17:02 +0100

    refactor: nuke useless variable

--- __tests__/tests/others/locales.js
+++ __tests__/tests/others/locales.js
@@ -3,13 +3,15 @@ import keysDiff from 'keys-diff';
 import * as languages from 'locale/languages';
 
 const baseLanguage = common.defaultLocale;
-const baseTranslation = languages[baseLanguage];
 
 describe('Locales', () => {
   Object.keys(languages).forEach(key => {
     if (key === baseLanguage) return;
     it(`${key} should contain same keys as ${baseLanguage}`, () => {
-      expect(keysDiff(baseTranslation, languages[key])).toEqual([[], []]);
+      expect(keysDiff(languages[baseLanguage], languages[key])).toEqual([
+        [],
+        [],
+      ]);
     });
   });
 });
