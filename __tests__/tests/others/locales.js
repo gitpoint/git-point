2a46167ceaf46581a09af9cedec038342190145b
Mehdi Achour <machour@gmail.com>
2018-02-06 17:39:27 +0100

    test: make locale test output more readable (#712)
    * test: make locale test output more readable
    
    * refactor: nuke useless variable

--- __tests__/tests/others/locales.js
+++ __tests__/tests/others/locales.js
@@ -1,28 +1,17 @@
 import { common } from 'config';
+import keysDiff from 'keys-diff';
 import * as languages from 'locale/languages';
 
-function createExpectedObject(object) {
-  const ret = {};
-
-  Object.keys(object).forEach(key => {
-    ret[key] =
-      typeof object[key] === 'object'
-        ? createExpectedObject(object[key])
-        : expect.stringMatching(/./);
-  });
-
-  return ret;
-}
-
 const baseLanguage = common.defaultLocale;
-const expectedObject = createExpectedObject(languages[baseLanguage]);
 
 describe('Locales', () => {
   Object.keys(languages).forEach(key => {
     if (key === baseLanguage) return;
-
     it(`${key} should contain same keys as ${baseLanguage}`, () => {
-      expect(languages[key]).toMatchObject(expectedObject);
+      expect(keysDiff(languages[baseLanguage], languages[key])).toEqual([
+        [],
+        [],
+      ]);
     });
   });
 });
