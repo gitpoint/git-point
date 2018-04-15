18aca3f05d22460c214cdb8eb621c05c81ef1a3f
Mehdi Achour <machour@gmail.com>
2018-04-15 15:05:13 +0100

    test: update tests to the new structure, use deep comparison

--- __tests__/tests/others/locales.js
+++ __tests__/tests/others/locales.js
@@ -1,17 +1,18 @@
+import { dumpKeysRecursively } from 'recursive-keys';
 import { common } from 'config';
-import keysDiff from 'keys-diff';
-import * as languages from 'locale/languages';
+import languages from 'locale/languages';
 
 const baseLanguage = common.defaultLocale;
 
 describe('Locales', () => {
+  const baseKeys = dumpKeysRecursively(languages[baseLanguage]).sort();
+
   Object.keys(languages).forEach(key => {
     if (key === baseLanguage) return;
     it(`${key} should contain same keys as ${baseLanguage}`, () => {
-      expect(keysDiff(languages[baseLanguage], languages[key])).toEqual([
-        [],
-        [],
-      ]);
+      expect(dumpKeysRecursively(languages[key]).sort()).toEqual(
+        expect.arrayContaining(baseKeys)
+      );
     });
   });
 });
