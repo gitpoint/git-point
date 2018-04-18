cc6850ec8e1980074dfa9cb1dff2ee208433b0a2
Mehdi Achour <machour@gmail.com>
2018-04-18 23:18:22 +0100

    refactor: redo relative time translations

--- src/utils/localization-helper.js
+++ src/utils/localization-helper.js
@@ -4,11 +4,8 @@ import distanceInWords from 'date-fns/distance_in_words';
 import { common } from 'config';
 import I18n from 'locale';
 
-export const translate = (key, locale, interpolation = null) =>
-  I18n.t(key, { locale, ...interpolation });
-
 export const t = (key, locale, interpolation = null) => {
-  let translation = translate(key, locale, interpolation);
+  let translation = I18n.t(key, locale, interpolation);
 
   if (translation === '') {
     translation = key;
@@ -43,6 +40,10 @@ export const t = (key, locale, interpolation = null) => {
 
   if (ongoing.length) {
     retval.push(ongoing);
+  }
+
+  if (retval.length === 1) {
+    return retval[0];
   }
 
   return retval;
@@ -93,8 +94,40 @@ export function relativeTimeToNow(date) {
   // https://github.com/date-fns/date-fns/blob/v1.29.0/src/locale/en/build_distance_in_words_locale/index.js
   const localeConfig = {
     distanceInWords: {
-      localize: (token, count) =>
-        translate(`common.relativeTime.${token}`, locale, { count }),
+      localize: (token, count) => {
+        switch (token) {
+          case 'xSeconds':
+            return t('{{xSeconds}}s', locale, { xSeconds: count });
+          case 'halfAMinute':
+            return t('{{halfAMinute}}s', locale, { halfAMinute: count });
+          case 'lessThanXMinutes':
+            return t('{{lessThanXMinutes}}m', locale, {
+              lessThanXMinutes: count,
+            });
+          case 'xMinutes':
+            return t('{{xMinutes}}m', locale, { xMinutes: count });
+          case 'aboutXHours':
+            return t('{{aboutXHours}}h', locale, { aboutXHours: count });
+          case 'xHours':
+            return t('{{xHours}}h', locale, { xHours: count });
+          case 'xDays':
+            return t('{{xDays}}d', locale, { xDays: count });
+          case 'aboutXMonths':
+            return t('{{aboutXMonths}}mo', locale, { aboutXMonths: count });
+          case 'xMonths':
+            return t('{{xMonths}}mo', locale, { xMonths: count });
+          case 'aboutXYears':
+            return t('{{aboutXYears}}y', locale, { aboutXYears: count });
+          case 'xYears':
+            return t('{{xYears}}y', locale, { xYears: count });
+          case 'overXYears':
+            return t('{{overXYears}}y', locale, { overXYears: count });
+          case 'almostXYears':
+            return t('{{almostXYears}}y', locale, { almostXYears: count });
+          default:
+            return '?';
+        }
+      },
     },
   };
 
