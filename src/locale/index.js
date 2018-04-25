29f5c328e3adfda1be10b4c275b45a6a9d5bb4c2
Mehdi Achour <machour@gmail.com>
2018-04-25 16:30:51 +0100

    fix: disable i18n defaultSeparator

--- src/locale/index.js
+++ src/locale/index.js
@@ -6,5 +6,6 @@ I18n.fallbacks = true;
 I18n.defaultLocale = common.defaultLocale;
 I18n.missingTranslation = scope => scope;
 I18n.translations = translations;
+I18n.defaultSeparator = 'GITPOINT-DISABLED';
 
 export default I18n;
