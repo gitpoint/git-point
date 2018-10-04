376517c87a24bb5a8f860762b4627fddbf9ac180
Richie <whitedogg13@gmail.com>
2018-10-04 22:14:13 +0800

    test: add more test cases against edge cases for GithubHtmlView

--- __tests__/tests/components/github-htmlview.js
+++ __tests__/tests/components/github-htmlview.js
@@ -8,7 +8,7 @@ describe('<GithubHtmlView />', () => {
   it('correctly renders <details/> tag in GithubHtmlView', () => {
     const sourceHtml = `
       <details>
-        <summary>  title  </summary>
+        <summary>title</summary>
         <div>description</div>
       </details> 
     `;
@@ -20,11 +20,64 @@ describe('<GithubHtmlView />', () => {
     // we should be able to find a <Icon/> with proper name,
     // which means our custom logic for <details/> is applied
     expect(inst.root.findByType(Icon).props.name).toEqual('triangle-right');
+  });
+
+  it('if <summary/> tag contains a single text node, we shoud prettify it', () => {
+    const sourceHtml = `
+      <details>
+        <summary>    
+              make some space and line break here...     
+        
+        </summary>
+        <div>description</div>
+      </details> 
+    `;
+
+    const inst = renderer.create(
+      <GithubHtmlView source={sourceHtml} onLinkPress={() => 0} />
+    );
 
     // we should be able to find the processed title text node,
     // which means our custom logic for <details/> is applied
     expect(
-      !!inst.root.findAllByType(Text).find(e => e.props.children === 'title')
+      !!inst.root
+        .findAllByType(Text)
+        .find(
+          e => e.props.children === 'make some space and line break here...'
+        )
     ).toBe(true);
+  });
+
+  it('if there is no <summary/> tag, we should do fallback render', () => {
+    const sourceHtml = `
+      <details>
+        <div>no summary here!!</div>
+      </details> 
+    `;
+
+    const inst = renderer.create(
+      <GithubHtmlView source={sourceHtml} onLinkPress={() => 0} />
+    );
+
+    // fallback render in this case won't contain any Icon
+    expect(inst.root.findAllByType(Icon).length).toBe(0);
+  });
+
+  it('if <summary/> contain nested tags, should still be rendered without exception', () => {
+    const sourceHtml = `
+      <details>
+        <summary>
+          abcde <div>test</div> fg
+        </summary>
+        <div>no summary here!!</div>
+      </details> 
+    `;
+
+    const inst = renderer.create(
+      <GithubHtmlView source={sourceHtml} onLinkPress={() => 0} />
+    );
+
+    // should still reach here, and correct render the right arrow icon
+    expect(inst.root.findByType(Icon).props.name).toEqual('triangle-right');
   });
 });
