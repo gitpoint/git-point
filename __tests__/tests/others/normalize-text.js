ffdbc9088cbac91f2a4bfea52ac3cc4ba63d66de
Xianming Zhong <chinesedfan@qq.com>
2019-10-25 22:43:22 +0800

    test: jest.isolateModules works

--- __tests__/tests/others/normalize-text.js
+++ __tests__/tests/others/normalize-text.js
@@ -9,77 +9,74 @@ const mockRequiredClasses = mockValues => {
   jest.mock('PixelRatio', () => mockClassWithGetter(mockValues.pixelRatio));
 };
 
+const expectSize = size => {
+  jest.isolateModules(() => {
+    const { normalize } = require('config');
+    expect(normalize(1)).toEqual(size);
+  });
+};
+
 describe('Normalize Text', () => {
   // iOS Devices
 
   it('should normalize correctly on iPhone 5', () => {
     mockRequiredClasses({ dimensions: screens.iPhone5, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPhone 6', () => {
     mockRequiredClasses({ dimensions: screens.iPhone6, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(1.15);
   });
 
   it('should normalize correctly on iPhone 6 Plus', () => {
     mockRequiredClasses({ dimensions: screens.iPhone6Plus, pixelRatio: 2 });
 
-    const { normalize } = require('config/normalize-text');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPhone 7', () => {
     mockRequiredClasses({ dimensions: screens.iPhone7, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPhone 7 Plus', () => {
     mockRequiredClasses({ dimensions: screens.iPhone7Plus, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPod Touch', () => {
     mockRequiredClasses({ dimensions: screens.iPodTouch, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPad Pro', () => {
     mockRequiredClasses({ dimensions: screens.iPadPro, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPad Gen 3/4', () => {
     mockRequiredClasses({ dimensions: screens.iPadGen3, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPad Air', () => {
     mockRequiredClasses({ dimensions: screens.iPadAir, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on iPad Mini', () => {
     mockRequiredClasses({ dimensions: screens.iPadMini, pixelRatio: 2 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   // Android Devices
@@ -87,29 +84,25 @@ describe('Normalize Text', () => {
   it('should normalize correctly on Nexus 6P', () => {
     mockRequiredClasses({ dimensions: screens.Nexus6P, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Nexus 5X', () => {
     mockRequiredClasses({ dimensions: screens.Nexus5X, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Google Pixel', () => {
     mockRequiredClasses({ dimensions: screens.GooglePixel, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Google Pixel XL', () => {
     mockRequiredClasses({ dimensions: screens.GooglePixelXL, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Samsung Galaxy Note 5', () => {
@@ -118,15 +111,13 @@ describe('Normalize Text', () => {
       pixelRatio: 3,
     });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Samsung Galaxy S7', () => {
     mockRequiredClasses({ dimensions: screens.SamsungGalaxyS7, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Samsung Galaxy S7 Edge', () => {
@@ -135,36 +126,31 @@ describe('Normalize Text', () => {
       pixelRatio: 3,
     });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on LG G5', () => {
     mockRequiredClasses({ dimensions: screens.LGG5, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on OnePlus 3', () => {
     mockRequiredClasses({ dimensions: screens.OnePlus3, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Nexus 7', () => {
     mockRequiredClasses({ dimensions: screens.Nexus7, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Nexus 9', () => {
     mockRequiredClasses({ dimensions: screens.Nexus9, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on Samsung Galaxy Tab 10', () => {
@@ -173,14 +159,12 @@ describe('Normalize Text', () => {
       pixelRatio: 3,
     });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 
   it('should normalize correctly on ChromebookPixel', () => {
     mockRequiredClasses({ dimensions: screens.ChromebookPixel, pixelRatio: 3 });
 
-    const { normalize } = require('config');
-    expect(normalize(1)).toEqual(0.95);
+    expectSize(0.95);
   });
 });
